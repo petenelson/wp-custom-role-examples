fa56d93892d88ad32308c4f85153f095810fecc6
Pete Nelson <pete@petenelson.com>
2017-03-17 13:22:05 -0500

    uncommented some code

--- includes/roles/hr.php
+++ includes/roles/hr.php
@@ -15,7 +15,7 @@ namespace Custom_Role_Examples\Roles\HR;
 function setup() {
 
 	// Hooks for the WP plugins_loaded action.
-	// add_action( 'admin_init',      __NAMESPACE__ . '\add_hr_role' );
+	add_action( 'admin_init',      __NAMESPACE__ . '\add_hr_role' );
 
 	// Hooks for user_has_cap filter.
 	// add_filter( 'user_has_cap',        __NAMESPACE__ . '\add_additional_caps', 10, 4 );
